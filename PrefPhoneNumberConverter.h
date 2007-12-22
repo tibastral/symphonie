2e8469e9c955d4e06adba2eb8d2394a20d8ef446
danielbraun <danielbraun@57038eb5-193d-0410-9cfd-c93977fb03f4>
2007-12-22 14:04:30 +0000

    files were missing
    git-svn-id: http://svn.assembla.com/svn/symphonie/trunk@67 57038eb5-193d-0410-9cfd-c93977fb03f4

---
+++ PrefPhoneNumberConverter.h
@@ -0,0 +1,25 @@
+//
+//  PrefPhoneNumberConverter.h
+//  sipPhone
+//
+//  Created by Daniel Braun on 10/12/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "StringCallNumber.h"
+
+@interface PrefPhoneNumberConverter : PhoneNumberConverter {
+
+}
+
+- (void) setConverterDef:(NSArray *)def;
+- (void) setInternationalPrefix:(NSString *)num;	// e.g; +33
+- (void) setNationalPrefix:(NSString *)n;		// e.g. 0
+- (NSString *) nationalPrefix;
+- (NSString *) internationalPrefix;
+- (NSArray *) converterDef;
+
++ (NSArray *) defaultPrefValue;
+@end
---
+++ PrefPhoneNumberConverter.m
@@ -0,0 +1,81 @@
+//
+//  PrefPhoneNumberConverter.m
+//  sipPhone
+//
+//  Created by Daniel Braun on 10/12/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "PrefPhoneNumberConverter.h"
+#import "Properties.h"
+
+@implementation PrefPhoneNumberConverter
+
+static NSArray *defRules=nil;
+
+#define _R(_s) [NSDictionary dictionaryWithObject:(_s) forKey:@"rule"]
+
++ (NSArray *) defaultPrefValue
+{
+	if (!defRules) {
+		defRules=[[NSArray arrayWithObjects:
+			   _R(@"I # ## ## ## ##"),
+			   _R(@"0 82# ### ###"),
+			   _R(@"I 82# ### ###"),
+			   _R(@"## ## ## ## ##"),
+			   _R(@"## ## ## ## #"),
+			   _R(@"## ## ## ##"),
+			   _R(@"## ## ## #"),
+			   _R(@"## ## ##"),
+			   _R(@"## ## #"),
+			   _R(@"## ##"),
+			   _R(@"###"),
+			   NULL]retain];
+	}
+	return defRules;
+}
+
+- (id) init
+{
+	self = [super init];
+	if (self != nil) {
+	}
+	return self;
+}
+
+- (void) setConverterDef:(NSArray *)def
+{
+	NSAssert(0, @"should not be used\n");
+}
+- (void) setInternationalPrefix:(NSString *)num
+{
+	setProp(@"numberInternationalPrefix", num);
+}
+- (void) setNationalPrefix:(NSString *)n
+{
+	setProp(@"numberNationalPrefix", n);
+}
+- (NSString *) nationalPrefix
+{
+	return getProp(@"numberNationalPrefix", @"0");
+}
+- (NSString *) internationalPrefix
+{
+	return getProp(@"numberInternationalPrefix", @"+33");
+}
+- (NSArray *) converterDef
+{
+	NSArray *p=getProp(@"numberRulesPref", [PrefPhoneNumberConverter defaultPrefValue]);
+	int i, count = [p count];
+	NSMutableArray *res=[NSMutableArray arrayWithCapacity:count];
+
+	for (i = 0; i < count; i++) {
+		NSDictionary * dr = [p objectAtIndex:i];
+		NSString *r=[dr objectForKey:@"rule"];
+		[res addObject:r];
+	}
+	return res;
+}
+
+
+@end
